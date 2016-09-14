01fefcf3a076cbdea259cc204d05bcc40490e181
uniceub <everton-cs@hotmail.com.br>
2016-09-14 15:15:31 -0300

    Classe request_controller

--- aula4/request_controller.php
+++ aula4/request_controller.php
@@ -4,7 +4,8 @@
 class RequestController
 {
 	const VALID_METHODS = array('GET', 'POST', 'PUT', 'DELETE');
-	const VALID_PROTOCOL = array('HTTP','HTTPS');
+	const VALID_PROTOCOL = array('HTTPS/1.1', 'HTTPS/1.0', 'HTTP/1.1', 'HTTP/1.0');
+	const VALID_PATHS = array('localhost/aula4/','localhost/aula4/index.php','localhost/aula4/request.php','localhost/aula4/request_controller.php');
 
 	public function create_request($request_info)
 	{
@@ -13,13 +14,20 @@ class RequestController
 			return array("code" => "405", "message" => "method not allowed");
 			
 		}
-		if(!self::is_valid_protocol(substr($request_info['SERVER_PROTOCOL'], 0, -4)))
+		if(!self::is_valid_protocol($request_info['SERVER_PROTOCOL']))
 		{
 			return array("code" => "505", "message" => "HTTP Version Not Supported");
 		}
-		return "Quer andar de meiota (888";
+		if(!self::is_valid_client_addr($request_info['REMOTE_ADDR']))
+		{
+			return array("code" => "400", "message" => "Bad Request");
+		}
+		if(!self::is_valid_path($request_info['HTTP_HOST'].$request_info['PHP_SELF']))
+		{
+			return array("code" => "400", "message" => "Invalid path");
+		}
 
-
+		return "pipoca";
 	//	$request_info['REMOTE_ADDR'];
 	//	$request_info['SERVER_ADDR'];
 	//	$request_info['SERVER_PROTOCOL'];
@@ -48,16 +56,21 @@ class RequestController
 		return true;
 	}
 
-
-
-
-
-
-
-
-
-
-
+	public function is_valid_client_addr($client_addr)
+	{
+		if(!filter_var($client_addr, FILTER_VALIDATE_IP)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public function is_valid_path($path)
+	{
+		if (is_null($path) || !in_array($path, self::VALID_PATHS))
+			return false;
+		
+		return true;
+	}
 
 
 
