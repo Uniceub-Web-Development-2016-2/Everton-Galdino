94293415cd988042beaf94d366824b0300663d5f
uniceub <everton-cs@hotmail.com.br>
2016-09-27 19:08:34 -0300

    conexão com bd

---
+++ aula6/db_manager.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class DBConnector extends PDO {
+   
+    private $engine;
+    private $host;
+    private $database;
+    private $user;
+    private $pass;
+    private $connector;	
+   
+    public function __construct(){
+        $this->engine = 'mysql';
+        $this->host = 'localhost';
+        $this->database = 'database';
+        $this->user = 'root';
+        $this->pass = '';
+        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
+        parent::__construct( $dns, $this->user, $this->pass );
+    }	
+		
+}
+
+//$result = (new DBConnector())->query($query);
+//var_dump($result->fetchAll());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
