7eb5e71562589e84751d43b37a1616c98098a3b8
uniceub <evertongladino@gmail.com>
2016-11-25 20:32:49 -0200

    atualizaçao

--- dotaez/client/index.php
+++ dotaez/client/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="img/dotaicone.jpeg">
 
     <title>DotaEz</title>
 
@@ -22,58 +22,37 @@
       <!-- The justified navigation menu is meant for single line per list item.
            Multiple lines will require custom code not provided by Bootstrap. -->
       <div class="masthead">
-        <h3 class="text-muted">Project name</h3>
+        <a href="http://localhost/dotaez/client/index.php"><img src="img/dotalogo.png" width="50" height="50"></a> 
         <nav>
           <ul class="nav nav-justified">
-            <li class="nav-item"><a class="nav-link active" href="http://localhost/dotaez/user">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Services</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Downloads</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+            <li class="nav-item"><a class="nav-link active" href="http://localhost/dotaez/client/index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Builds</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Heroes</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Itens</a></li>
+            <li class="nav-item"><a class="nav-link" href="http://localhost/dotaez/client/contact.html">Contact</a></li>
+	    <li class="nav-item"><a class="nav-link" href="#">Login(Moderadores)</a></li>
           </ul>
         </nav>
       </div>
 
       <!-- Jumbotron -->
       <div class="jumbotron">
-        <h1>Marketing stuff!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+        <h1>Atualização 6.87</h1>
+        <p class="lead">Tudo sobre a última atualização</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Saiba mais</a></p>
       </div>
 
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-lg-4">
-          <h2>Safari bug warning!</h2>
-          <p class="text-danger">As of v9.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
-
-      <!-- Site footer -->
+          <h2>Valve anuncia datas do próximo International</h2>
+          <p>A Valve anunciou nesta quinta-feira (3) as datas para a próxima edição do The International, o campeonato mundial de Dota 2, e novas informações para o Major da primavera norte-americana do ano que vem, incluindo os dias da competição e mudanças no formato.</p>
+          <p><a class="btn btn-primary" href="#" role="button">Veja mais &raquo;</a></p>
       <footer class="footer">
-        <p>&copy; Company 2016</p>
+        <p>&copy; Direito de resposta MC Jhonny(Esperando o silêncio pra rimar)</p>
       </footer>
-
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        </div>
+    </div>
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
     <a href='http://localhost/dotaez/user'>Buscar Usuarios</a><br>
